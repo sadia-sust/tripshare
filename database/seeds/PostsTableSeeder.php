d1ffd67feb9b9b1654a931478119bafcd77d4d0e
nowshad-sust <iamnowshad@gmail.com>
2017-03-16 22:28:34 +0600

    added two more seed

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -13,6 +13,40 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         Post::create([
+            
+            'photo_url' =>  'images/single-post-pic.jpg',
+            
+            'video_url' =>  null,
+            
+            'location'  =>  [
+                'name'  =>  'Backpack',
+                'lat'   =>  '90.01',
+                'long'  =>  '23.05'
+            ],
+
+            'user'      =>  [
+                            "_id" => "58ca33dc33181d38136ea5c1",
+                            "name" => "Nowshad",
+                            "email" => "iamnowshad@gmail.com",
+                            "updated_at" => "2017-03-16T06:42:36Z",
+                            "created_at" => "2017-03-16T06:42:36Z"
+                        ]
+,
+            'description'=> "Screen readers will have trouble with your forms if you don't include a label for every input. For these inline forms, you can hide the labels using the .sr-only class. There are further alternative methods of providing a label for assistive technologies, such as the aria-label, aria-labelledby or title attribute. If none of these is present, screen readers may resort to using the placeholder attribute, if present, but note that use of placeholder as a replacement for other labelling methods is not advised.",
+
+            'tags'      =>  [
+                                'backpack',
+                                'hackathon',
+                                'hack',
+                                'fun'
+                            ],
+            
+            'upvote'    =>  [],
+            
+            'downvote'  =>  []
+        ]);
+
+        Post::create([
         	'photo_url' =>	null,
         	'video_url'	=>	'https://www.youtube.com/watch?v=Scxs7L0vhZ4',
         	'location'	=>	[
@@ -31,6 +65,27 @@ class PostsTableSeeder extends Seeder
         					],
         	'upvote'	=>	[],
         	'downvote'	=>	[]
+        ]);
+
+        Post::create([
+            'photo_url' =>  null,
+            'video_url' =>  'https://www.youtube.com/watch?v=ScxvhZ4',
+            'location'  =>  [
+                'name'  =>  'Backpack',
+                'lat'   =>  '90.01',
+                'long'  =>  '23.05'
+            ],
+            'user'      =>  App\User::first(),
+            'description'=> "Screen readers will have trouble with your forms if you don't include a label for every input. For these inline forms, you can hide the labels using the .sr-only class. There are further alternative methods of providing a label for assistive technologies, such as the aria-label, aria-labelledby or title attribute. If none of these is present, screen readers may resort to using the placeholder attribute, if present, but note that use of placeholder as a replacement for other labelling methods is not advised.",
+
+            'tags'      =>  [
+                                'backpack',
+                                'hackathon',
+                                'hack',
+                                'fun'
+                            ],
+            'upvote'    =>  [],
+            'downvote'  =>  []
         ]);
     }
 }
